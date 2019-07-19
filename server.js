3e909d03b097efc5faa16fc820fdc4320adcb640
jessicakwalters <jessicakwalters@gmail.com>
2019-07-19 16:29:36 -0700

    check db for location data

--- server.js
+++ server.js
@@ -20,13 +20,15 @@ client.connect();
 client.on('error', err => console.error(err));
 
 //ROUTES
-app.get('/location', searchToLatLong);
+app.get('/location', (request, response) => {
+  getLocation(request.query.data)
+    .then(locationData => response.send(locationData))
+    .catch(error => handleError(error, response));
+});
 
 app.get('/weather', getWeather);
 
 app.get('/events', getEvents);
-
-
 
 // Error handler
 function handleError(err, res) {
@@ -36,25 +38,60 @@ function handleError(err, res) {
 
 //LOGIC
 
-function searchToLatLong(request, response) {
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`
-
-  return superagent.get(url)
-    .then((result) => {
-      const location = new Location(request.query.data, JSON.parse(result.text));
-      response.send(location);
-    })
-    .catch((error) => {
-      handleError(error);
-    });
-}
-
-function Location(query, res) {
+function Location(query, data) {
   this.search_query = query;
-  this.formatted_query = res.results[0].formatted_address;
-  this.latitude = res.results[0].geometry.location.lat;
-  this.longitude = res.results[0].geometry.location.lng;
+  this.formatted_query = data.formatted_address;
+  this.latitude = data.geometry.location.lat;
+  this.longitude = data.geometry.location.lng;
 }
+
+
+function getLocation(query) {
+
+  const SQL = `SELECT * FROM locations WHERE search_query=$1;`;
+  const values = [query];
+
+  return client.query(SQL, values)
+    .then((result) => {
+      if (result.rowCount > 0) {
+        console.log('From SQL');
+        return result.rows[0];
+      } else {
+        const _URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+        return superagent.get(_URL)
+
+          .then(data => {
+            console.log('FROM API');
+            if (!data.body.results.length) { throw 'No Data'; }
+            else {
+              let location = new Location(query, data.body.results[0]);
+              let NEWSQL = `INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES($1,$2,$3,$4) RETURNING id`;
+              let newValues = Object.values(location);
+              return client.query(NEWSQL, newValues)
+                .then(results => {
+                  location.id = results.rows[0].id;
+                  return location;
+                })
+                .catch(console.error);
+            }
+          });
+      }
+    })
+    .catch(console.error);
+}
+
+// function searchToLatLong(request, response) {
+//   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`
+
+//   return superagent.get(url)
+//     .then((result) => {
+//       const location = new Location(request.query.data, JSON.parse(result.text));
+//       response.send(location);
+//     })
+//     .catch((error) => {
+//       handleError(error);
+//     });
+// }
 
 function getWeather(request, response) {
   const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`
